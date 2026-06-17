067bfa9d4ce456a5352688075af6daa87b12eee2
Melih0vArtem <leontoptop85@gmail.com>
2026-06-17 15:52:42 +0300

    Add files via upload

---
+++ ПР_15/1/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnyes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Мы и не сомневались, что Вы так думаете!");
+        }
+
+        private void btnno_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnno.Top -= e.Y;
+            btnno.Left += e.X;
+            if (btnno.Top < -10 || btnno.Top > 100)
+                btnno.Top = 60;
+            if (btnno.Left < -80 || btnno.Left > 250)
+                btnno.Left = 120;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.Length != 0)
+            {
+                checkedListBox1.Items.Add(comboBox1.Text);
+            }
+            else MessageBox.Show("Выберите элемент из списка или введите новый");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            while (checkedListBox1.CheckedIndices.Count > 0)
+                checkedListBox1.Items.RemoveAt(checkedListBox1.CheckedIndices[0]);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Sorted = true;
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "NewDoc":
+                    ChildForm newChild = new ChildForm();
+                    newChild.Show();
+                    newChild.Text = newChild.Text + " ";
+                    break;
+                case "Cascade":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+                    break;
+                case "Title":
+                    this.LayoutMdi
+                    (System.Windows.Forms.MdiLayout.TileHorizontal);
+                    break;
+            }
+        }
+
+        private void toolStrip1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void spWin_Click(object sender, EventArgs e)
+        {
+            spWin.Text = "Windows is cascade";
+            spWin.Text = "Windows is horizontal";
+
+        }
+
+        private void spData_Click(object sender, EventArgs e)
+        {
+
+            spData.Text =
+            Convert.ToString(System.DateTime.Today.ToLongDateString());
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+                this.but.Text = "First";
+            else if (radioButton2.Checked == true)
+                this.but.Text = "Second";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.SetFlowBreak(button6, true);
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Button aButton = new Button();
+            tableLayoutPanel1.Controls.Add(aButton, 1, 1);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            splitContainer1.IsSplitterFixed = !(splitContainer1.IsSplitterFixed);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (splitContainer1.FixedPanel == FixedPanel.Panel1)
+                splitContainer1.FixedPanel = FixedPanel.None;
+            else
+                splitContainer1.FixedPanel = FixedPanel.Panel1;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            splitContainer1.Panel1Collapsed = !(splitContainer1.Panel1Collapsed);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.SetFlowBreak(button6, true);
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DialogResult aResult;
+            Form2 aForm = new Form2();
+            aResult = aForm.ShowDialog();
+            if (aResult == System.Windows.Forms.DialogResult.OK)
+                MessageBox.Show("Your address is " + aForm.textBox3.Text);
+            MessageBox.Show("Your phone number is " +
+            aForm.maskedTextBox1.Text);
+            {
+                MessageBox.Show("Your name is " + aForm.textBox1.Text + " " + aForm.textBox2.Text);
+            }
+            linkLabel1.LinkVisited = true;
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("www.limtu.com");
+            linkLabel2.LinkVisited = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked==true)
+            {
+                Label lbl = new Label();
+                lbl.Location = new System.Drawing.Point(16, 96);
+                lbl.Size = new System.Drawing.Size(32, 23);
+                lbl.Name = "labelll";
+                lbl.TabIndex = 2;
+                lbl.Text = "PIN2";
+                groupBox1.Controls.Add(lbl);
+                TextBox txt = new TextBox();
+                txt.Location = new System.Drawing.Point(96, 96);
+                txt.Size = new System.Drawing.Size(184, 20);
+                txt.Name = "textboxx";
+                txt.TabIndex = 1;
+                txt.Text = "";
+                groupBox1.Controls.Add(txt);
+            }
+            else
+            {
+                int lcv;
+                lcv = groupBox1.Controls.Count;// определяется количество
+                while (lcv > 4)
+                {
+                    groupBox1.Controls.RemoveAt(lcv - 1);
+                    lcv -= 1;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Поле Name не может содержать цифры");
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Поле PIN не может содержать буквы");
+            }
+
+        }
+
+        private void checkBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void textBox2_Validating(object sender, CancelEventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+            }
+            else
+            {
+                e.Cancel = false;
+                try
+                {
+                    double.Parse(textBox2.Text); e.Cancel = false;
+                }
+                catch
+                {
+                    e.Cancel = true;
+                    MessageBox.Show("Поле PIN не может содержать буквы");
+                }
+            }
+        }
+    }
+}
+
